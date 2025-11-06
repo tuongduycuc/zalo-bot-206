e0620d5e9e5248f60e1eda3ff614bddf2808590f
tuongduycuc <tuongduycuc@hotmail.com>
2025-11-06 11:24:41 +0700

    Update ACCESS_TOKEN with new value

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import fs from "fs";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const ACCESS_TOKEN = "ZALO_OA_ACCESS_TOKEN_CUA_BAN"; // 👈 Thay token OA thật vào đây
+const ACCESS_TOKEN = "XzfP9dlmHLU2rLegSVvCMOBLLYfdhYaYbTvjDntuS020dXLf3U9F7PAx5HGKxNbYffC1SXZuM6MOcqvxFhu3POBVJdC7fmHpjk1002wz81kFm45h5QO5MUtH4qyHZamudUHMAM2VEYRulbCHU-Wz7DMLV1Glu0mNXQfo6IQDBGUNm4iP2x0X3PBaTWGUl0y1eyPG0bwuBXRlmMivTeCT8jluK0LPZ4ODegmz4opiNXAYZXyRESHZ9hZc22mcn74cjO4JAZRcL1ISW3rZRzXQTlQiFbrKwKPpte4lHNtODYpFXauwRl0c4vRKSHDhYpSdoQejKs7YRK_VW7ffODutUDkFTaHsnJGrzU5p5tEcFGoLpYOVQSiIBqHuvaLL"; // 👈 Thay token OA thật vào đây
 const GROUP_ID = "GROUP_ID_CUA_NHOM"; // 👈 Lấy trong webhook khi nhận tin nhắn nhóm
 
 app.use(bodyParser.json());
